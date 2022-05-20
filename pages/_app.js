07e550dc933db9c6db7d83c3cd24e24d9f30086f
jdsteege <jdsteege@gmail.com>
2022-05-19 23:10:25 -0500

    Added fomantic ui css

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { Loader } from "semantic-ui-react";
 import "../styles/globals.css";
+import "fomantic-ui-css/semantic.min.css";
 
 //
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
